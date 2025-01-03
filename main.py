c80bb2983d851246847fd7328a016442cadf9eae
Ryan <rlowndes9@gmail.com>
2025-01-03 20:13:55 +0000

    Changed up code and added tick

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        # Handle quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()  # Add this line to properly close pygame
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
